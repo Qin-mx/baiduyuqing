a97a40c27e1d6daf008d32f53a47db6016660c1d
Qin-mx <1768912132@qq.com>
2016-06-22 17:46:01 +0800

    初始化

---
+++ js/index.js
@@ -0,0 +1,10 @@
+		$(function(){
+			$('#fullpage').fullpage({
+			navigation:true,
+			navigationPoaition:'right',
+			navigationTooltips:['登录百度舆情','领先：数据收集与处理','全面：舆情分析逻辑与架构','专业：专业数据可视化'],
+		});
+			$('.xiaojiantou').on('click',function(){
+		    $.fn.fullpage.moveSectionDown();
+		  })
+	})
